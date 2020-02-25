229585b1db2707636ff42df41626bc77a37f975e
Pedro Stradiotti <pedrostra.neto@gmail.com>
2020-02-25 18:16:51 -0300

    Finalizado Estrutura

---
+++ exercicios/entregaUm/js/script.js
@@ -0,0 +1,10 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById('btn2');
+
+btn1.addEventListener('click', () => {
+    window.open('https://slack.com/get-started#/', '_blank')
+});
+
+btn2.addEventListener('click', () => {
+    window.open('https://slack.com/get-started#/', '_blank')
+});
