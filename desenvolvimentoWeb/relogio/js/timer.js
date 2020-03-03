c8cfa9cc69c6368d4921ed20ad16eacf3023da50
PEDRO STRADIOTTI NETO <pedro.sneto6@senacsp.edu.br>
2020-03-03 11:12:43 -0300

    Relógio e Debugger

--- desenvolvimentoWeb/relogio/js/timer.js
+++ desenvolvimentoWeb/relogio/js/timer.js
@@ -1,1 +1,25 @@
+// innerHTML    -> Escreve/adiciona no mundo HTML
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Element/innerHTML
+
 let currentTime = document.getElementById('currtime');
+
+function ajustaValor(number) {
+    if (number < 10) {
+        number = "0" + number;
+    }
+    return number;
+}
+
+function updateTime() {
+    let timeNow = new Date(),
+        hh = timeNow.getHours(),
+        mm = timeNow.getMinutes(),
+        ss = timeNow.getSeconds()
+
+    currentTime.innerHTML = ajustaValor(hh) + '<span>:</span>' + ajustaValor(mm) + '<span>:</span>' + ajustaValor(ss);
+
+    setTimeout(updateTime, 1000);
+
+}
+
+updateTime();
